74d77421a9937d7f78c5aa5b53229c073b93d804
Owen Harvey <80526926+owenharvey7@users.noreply.github.com>
2024-10-10 09:01:51 -0400

    Create EmployeesEarningMoreThanManagers.sql

---
+++ Leetcode-Database/EmployeesEarningMoreThanManagers.sql
@@ -0,0 +1,19 @@
+/*
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write a solution to find the employees who earn more than their managers.
+*/
+
+select name as Employee
+from Employee as E1
+Where salary > (select salary from Employee as E2 where id=E1.managerId)
